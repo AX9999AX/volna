ef5160bb7543384aba2938e91808956b4d015127
Volodymyr Cherkashyn <volo.cherkashyn@gmail.com>
2025-05-01 01:38:47 +0300

    Fixed bugs

--- src/app/components/MegaGallery/Interior.tsx
+++ src/app/components/MegaGallery/Interior.tsx
@@ -357,23 +357,6 @@ const Interior = (props: IMegaGallery) => {
                                 sizes='50vw'
                             />
                         </motion.div>
-                        <motion.div
-                            initial={{ opacity: 0, scale: 0.95 }}
-                            animate={isInView ? { opacity: 1, scale: 1 } : {}}
-                            transition={{ duration: 0.6, delay: 0.3 }}
-                            className='cursor-pointer order-2 md:order-0 relative overflow-hidden min-h-[300px] rounded-[20px]'
-                            onClick={() => {
-                                setCurrentImage('/mega-gallery/interior-25.jpg')
-                                setIsModalOpen(true)
-                            }}>
-                            <Image
-                                fill
-                                src={'/mega-gallery/interior-25.jpg'}
-                                alt='Gallery 1'
-                                className='object-cover'
-                                sizes='50vw'
-                            />
-                        </motion.div>
                     </>
                 )}
             </div>
